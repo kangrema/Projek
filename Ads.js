a8403118478428ae3dae3b412cfdad108099b8ce
kangrema <37550690+kangrema@users.noreply.github.com>
2018-03-20 10:18:29 +0700

    Ads

---
+++ Ads.js
@@ -0,0 +1,3 @@
+ads = new Array(1);
+ads [0] = '<div class="adscloud-box"><a href="http://linkiklan" target="_blank"><div class="adscloud1">Judul iklan</div></a><div class="adscloud-desc">Deskripsi iklan</div><a href="http://linkiklan" target="_blank"><div class="adscloud2">homepage situs contoh www.radarseru.com</div></a><p class="ad"><a href="http://www.adscloud.biz/" target="_blank" title="AdsCloud.biz">Ads By AdsCloud</a></p></div>';
+view = Math.floor(Math.random() * 1); document.write(ads[view]);
