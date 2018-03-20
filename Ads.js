9cc9d7f1be85437aee7f853cc3f09b41e7ac69cf
kangrema <37550690+kangrema@users.noreply.github.com>
2018-03-20 10:22:47 +0700

    Update Ads.js

--- Ads.js
+++ Ads.js
@@ -1,3 +1,8 @@
-ads = new Array(1);
-ads [0] = '<div class="adscloud-box"><a href="http://linkiklan" target="_blank"><div class="adscloud1">Judul iklan</div></a><div class="adscloud-desc">Deskripsi iklan</div><a href="http://linkiklan" target="_blank"><div class="adscloud2">homepage situs contoh www.radarseru.com</div></a><p class="ad"><a href="http://www.adscloud.biz/" target="_blank" title="AdsCloud.biz">Ads By AdsCloud</a></p></div>';
+ads = new Array(
+);
+ads [0] = '<div class="adscloud-box"><a href="http://linkiklan" target="_blank"><div class="adscloud">Judul iklan</div></a><div class="adscloud-desc">Deskripsi iklan</div><a href="http://linkiklan" target="_blank"><div class="adscloud2">homepage situs contoh www.radarseru.com</div></a><p class="ad"><a href="http://www.adscloud.biz/" target="_blank" title="AdsCloud.biz">Ads By AdsCloud</a></p></div>';
 view = Math.floor(Math.random() * 1); document.write(ads[view]);
+
+ads = new Array(2);
+ads [2] = '<div class="adscloud-box"><a href="http://linkiklan" target="_blank"><div class="adscloud">Judul iklan</div></a><div class="adscloud-desc">Deskripsi iklan</div><a href="http://linkiklan" target="_blank"><div class="adscloud2">homepage situs contoh www.radarseru.com</div></a><p class="ad"><a href="http://www.adscloud.biz/" target="_blank" title="AdsCloud.biz">Ads By AdsCloud</a></p></div>';
+view = Math.floor(Math.random() * 2); document.write(ads[view]);
